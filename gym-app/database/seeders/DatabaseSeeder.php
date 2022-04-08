242e8477753f76e75d603a3daca11c85746280ae
vegh-barnabas <akg4s@inf.elte.hu>
2022-04-08 20:57:11 +0200

    Make User fms

--- gym-app/database/seeders/DatabaseSeeder.php
+++ gym-app/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +18,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        /* Users */
+        DB::table('users')->truncate();
+
+        // Admin
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@br.hu',
+            'password' => Hash::make('password'),
+            'is_admin' => true,
+        ]);
+
+        // Receptionists
+        for ($i = 1; $i <= rand(2, 4); $i++) {
+            User::factory()->create([
+                'name' => 'receptionist' . $i,
+                'email' => 'receptionist'. $i .'@br.hu',
+                'password' => Hash::make('password'),
+                'is_receptionist' => true,
+            ]);
+        }
+
+        // Users
+        for ($i = 1; $i <= rand(3, 7); $i++) {
+            User::factory()->create([
+                'name' => 'user'. $i,
+                'email' => 'user'. $i . '@br.hu',
+                'password' => Hash::make('password'),
+            ]);
+        }
     }
 }
