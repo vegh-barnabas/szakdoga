6a78260e5a30cd6af728804b77e0ee99e5584a20
vegh-barnabas <akg4s@inf.elte.hu>
2022-04-29 17:52:21 +0200

    make web.php routes ordered

--- gym-app/routes/web.php
+++ gym-app/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 use App\Models\Gym;
 use App\Models\Enterance;
@@ -23,130 +25,12 @@ use App\Models\User;
 
 Auth::routes();
 
-Route::get('/', function () {
-    return view('gyms.index', ['gyms' => Gym::all()]);
-})->name('gyms')->middleware('auth');
+// TODO: make error texts, check input validation
 
-Route::post('/', function (Request $request) {
-    if(!is_numeric($request['gymId'])) return redirect()->route('gyms');
 
-    if(Gym::all()->pluck('id')->contains($request['gymId'])) {
-        session(['gym' => $request['gymId']]);
-        return redirect()->route('index');
-    }
-    else return redirect()->route('gyms')->middleware('auth');
-});
+/* --- Mutual Routes --- */
 
-Route::get('/let-in', function (Request $request) {
-    if(Auth::user()->is_receptionist) {
-        return view('receptionist.let-in');
-    }
-})->name('let-in')->middleware('auth');
-
-Route::post('/let-in', function (Request $request) {
-    if(Auth::user()->is_receptionist) {
-
-        // $validated = $request->validate(
-        //     // Validation rules
-        //     [
-        //         // 'enterance_code' => 'required',
-        //         // '' => 'required|accepted'
-        //     ],
-        // );
-
-        // error_log($request->input('enterance_code'));
-
-        $code = $request->input('enterance_code');
-
-        return redirect()->route('let-in-2', $code);
-    }
-})->name('let-in')->middleware('auth');
-
-Route::get('/let-in/{code}', function (Request $request, $code) {
-    if(Auth::user()->is_receptionist) {
-        $ticket = Ticket::all()->where('code', $code)->first();
-
-        if($ticket === null) return view('receptionist.let-in'); // TODO: error message
-        if($ticket->exit !== null) return view('receptionist.let-in'); // TODO: error message
-
-        $user = $ticket->user;
-
-        $lockers = Locker::all()->where('gender', $user->gender)->where('user_id', null);
-
-        return view('receptionist.let-in-2', ['user' => $user, 'ticket' => $ticket, 'lockers' => $lockers, 'code' => $code]);
-    }
-})->name('let-in-2')->middleware('auth');
-
-Route::post('/let-in/{code}', function (Request $request, $code) {
-    if(Auth::user()->is_receptionist) {
-
-        // error_log($request->input('locker'));
-        // error_log($request->input('keyGiven'));
-
-        $ticket = Ticket::all()->where('code', $code)->first();
-        $user = $ticket->user;
-        $lockers = Locker::all()->where('gender', $user->gender)->where('user_id', null);
-
-        $validated = $request->validate(
-            // Validation rules
-            [
-                'locker' => 'required', // TODO: check if locker is in lockers
-                'keyGiven' => 'required|accepted',
-            ],
-        );
-
-        $locker = Locker::all()->where('number', $validated['locker']);
-
-        $user->locker = $locker;
-
-        // TODO: create enterance with ticket
-        $enter_time = new DateTime();
-
-        $new_enterance = Enterance::factory()->create([
-            'gym_id' => $ticket->gym_id,
-            'user_id' => $user->id,
-            'ticket_id' => $ticket->id,
-            'enter' => $enter_time,
-            'exit' => null
-        ]);
-
-        return redirect()->route('let-in');
-    }
-})->name('let-in-2')->middleware('auth');
-
-Route::get('/let-out/{code}', function (Request $request, $code) {
-    if(Auth::user()->is_receptionist) {
-        $user = User::all()->where('exit_code', $code)->first();
-        if($user === null) return view('receptionist.let-out'); // TODO: error message
-
-        $enterace = $user->enterances()->sortByDesc('enter')->first();
-        if($enterace->exit != null) return view('receptionist.let-out'); // TODO: error message
-
-        $exit_time = new DateTime();
-        $enterance->exit = $exit_time;
-
-        return redirect->route('let-out-2');
-    }
-})->name('let-out-2')->middleware('auth');
-
-Route::post('/let-out/{code}', function (Request $request, $code) {
-    if(Auth::user()->is_receptionist) {
-        return view('receptionist.let-out');
-    }
-})->name('let-out-2')->middleware('auth');
-
-Route::get('/let-out/{code}', function (Request $request, $code) {
-    if(Auth::user()->is_receptionist) {
-        return view('receptionist.let-out-2');
-    }
-})->name('let-out-2')->middleware('auth');
-
-Route::get('/settings', function (Request $request) {
-    if(Auth::user()->is_receptionist) {
-        return view('receptionist.let-in');
-    }
-})->name('settings')->middleware('auth');
-
+// Home - User & Receptionist
 Route::get('/home', function () {
     if(session('gym') == null) return redirect()->route('gyms');
 
@@ -188,12 +72,43 @@ Route::get('/home', function () {
     }
 })->name('index')->middleware('auth');
 
+// Settings - User & Receptionist
+Route::get('/settings', function (Request $request) {
+    if(Auth::user()->is_receptionist) return view('receptionist.settings');
+    else return view('user.settings');
+})->name('settings')->middleware('auth');
+
+/* --- User Routes --- */
+
+// Choose Gym
+Route::get('/', function () {
+    return view('gyms.index', ['gyms' => Gym::all()]);
+})->name('gyms')->middleware('auth');
+
+// Gym Index
+Route::post('/', function (Request $request) {
+    // TODO: check if this validation redicect is working
+    $validated = $request->validate([
+        'gymId' => [
+            'required',
+            'numeric',
+            Rule::in(Gym::all()->pluck('id'))
+        ]
+    ]);
+
+    session(['gym' => $validated['gymId']]);
+
+    return redirect()->route('index');
+});
+
+// Buy Ticket
 Route::get('/buy', function () {
     $gym = Gym::find(session('gym'));
 
     return view('user.buy', ['gym' => $gym]);
-})->name('buyticketpage')->middleware('auth');
+})->name('buyticketpage')->middleware('auth'); // TODO: rename this to buy_ticket
 
+// Check Tickets
 Route::get('/tickets', function () {
     if(session('gym') == null) return redirect()->route('gyms');
 
@@ -213,72 +128,7 @@ Route::get('/tickets', function () {
 
 })->name('tickets')->middleware('auth');
 
-
-Route::get('/buy/{ticket}', function (Request $request, $buyable_ticket_id) {
-    if(session('gym') == null) return redirect()->route('gyms');
-
-    if(Auth::user()->is_receptionist) return redirect()->route('index');
-
-    $ticket = BuyableTicket::find($buyable_ticket_id);
-
-    $ticket_type = $ticket->type == "bérlet" ? "Bérlet" : "Jegy";
-
-    return view('user.buy_ticket', ['ticket' => $ticket, 'ticket_type' => $ticket_type]);
-})->name('buy_ticket')->middleware('auth');
-
-Route::post('/buy/{ticket}', function (Request $request, $buyable_ticket_id) {
-    $buyable_ticket = BuyableTicket::find($buyable_ticket_id);
-
-    $current_date = new DateTime();
-    $expiration = new DateTime();
-    $expiration->modify("+1 month");
-
-    $new_ticket = Ticket::factory()->create([
-        'user_id' => Auth::user()->id,
-        'gym_id' => $buyable_ticket->gym_id,
-        'type_id' => $buyable_ticket->id,
-        'bought' => $current_date,
-        'expiration' => $expiration
-    ]);
-
-    return redirect()->route('index');
-})->name('buy_ticket')->middleware('auth');
-
-
-Route::get('/extend/{ticket}', function (Request $request, Ticket $ticket) {
-    if(session('gym') == null) return redirect()->route('gyms');
-
-    if(Auth::user()->is_receptionist) return redirect()->route('index');
-
-    if(Auth::user()->tickets->where('id', $ticket->id)->count() == 0) return redirect()->route('index');
-
-    if($ticket->expiration >= date('Y-m-d H:i:s')) return redirect()->route('tickets');
-
-    return view('user.extend_ticket', ['ticket' => $ticket]);
-})->name('extend_ticket')->middleware('auth');
-
-Route::post('/extend/{ticket}', function (Request $request, Ticket $ticket) {
-    if(session('gym') == null) return redirect()->route('gyms');
-
-    if(Auth::user()->is_receptionist) return redirect()->route('index');
-
-    if(Auth::user()->tickets->where('id', $ticket->id)->count() == 0) return redirect()->route('index');
-
-    if(strtotime($ticket->expiration) >= date('Y-m-d H:i:s')) return redirect()->route('tickets');
-
-    if(Auth::user()->credits < $ticket->type->price) return redirect()->route('tickets'); // TODO: error
-
-    $expiration = new DateTime();
-    $expiration->modify("+1 month");
-    $ticket->expiration = $expiration;
-    $ticket->save();
-
-    Auth::user()->credits -= $ticket->type->price;
-    Auth::user()->save();
-
-    return redirect()->route('index');
-})->name('extend_ticket')->middleware('auth');
-
+// Show Staticstics
 Route::get('/stats', function () {
     $gym = Gym::find(session('gym'));
 
@@ -297,6 +147,7 @@ Route::get('/stats', function () {
         }
     }
 
+    // TODO: make this into a function in model
     // Enterance avg
     $min_sum = 0;
     $count = 0;
@@ -321,3 +172,195 @@ Route::get('/stats', function () {
 
     return view('user.statistics', ['gym' => $gym, 'hour_avg' => $hours, 'min_avg' => $minutes, 'people_inside' => $people_inside]);
 })->name('userstats')->middleware('auth');
+
+// Extend Ticket
+Route::get('/extend/{ticket}', function (Request $request, Ticket $ticket) {
+    if(session('gym') == null) return redirect()->route('gyms');
+
+    if(Auth::user()->is_receptionist) return redirect()->route('index');
+
+    if(Auth::user()->tickets->where('id', $ticket->id)->count() == 0) return redirect()->route('index');
+
+    if($ticket->expiration >= date('Y-m-d H:i:s')) return redirect()->route('tickets');
+
+    return view('user.extend_ticket', ['ticket' => $ticket]);
+})->name('extend_ticket')->middleware('auth');
+
+// Extend Ticket POST
+Route::post('/extend/{ticket}', function (Request $request, Ticket $ticket) {
+    if(session('gym') == null) return redirect()->route('gyms');
+
+    if(Auth::user()->is_receptionist) return redirect()->route('index');
+
+    if(Auth::user()->tickets->where('id', $ticket->id)->count() == 0) return redirect()->route('index');
+
+    if(strtotime($ticket->expiration) >= date('Y-m-d H:i:s')) return redirect()->route('tickets');
+
+    if(Auth::user()->credits < $ticket->type->price) return redirect()->route('tickets'); // TODO: error
+
+    $expiration = new DateTime();
+    $expiration->modify("+1 month");
+    $ticket->expiration = $expiration;
+    $ticket->save();
+
+    Auth::user()->credits -= $ticket->type->price;
+    Auth::user()->save();
+
+    return redirect()->route('index');
+})->name('extend_ticket')->middleware('auth');
+
+// Buy Ticket
+Route::get('/buy/{ticket}', function (Request $request, $buyable_ticket_id) {
+    if(session('gym') == null) return redirect()->route('gyms');
+
+    if(Auth::user()->is_receptionist) return redirect()->route('index');
+
+    $ticket = BuyableTicket::find($buyable_ticket_id);
+
+    $ticket_type = $ticket->type == "bérlet" ? "Bérlet" : "Jegy";
+
+    return view('user.buy_ticket', ['ticket' => $ticket, 'ticket_type' => $ticket_type]);
+})->name('buy_ticket')->middleware('auth');
+
+// Buy Ticket POST
+Route::post('/buy/{ticket}', function (Request $request, $buyable_ticket_id) {
+    $buyable_ticket = BuyableTicket::find($buyable_ticket_id);
+
+    $current_date = new DateTime();
+    $expiration = new DateTime();
+    $expiration->modify("+1 month");
+
+    $new_ticket = Ticket::factory()->create([
+        'user_id' => Auth::user()->id,
+        'gym_id' => $buyable_ticket->gym_id,
+        'type_id' => $buyable_ticket->id,
+        'bought' => $current_date,
+        'expiration' => $expiration
+    ]);
+
+    return redirect()->route('index');
+})->name('buy_ticket')->middleware('auth');
+
+/* --- Receptionist Routes --- */
+
+// Let-in index page
+Route::get('/let-in', function () {
+    if (!Auth::user()->is_receptionist) abort(403);
+
+    return view('receptionist.let-in');
+})->name('let-in')->middleware('auth');
+
+// Let-in index page POST
+Route::post('/let-in', function (Request $request) {
+    if (!Auth::user()->is_receptionist) abort(403);
+
+    $validated = $request->validate(
+        [
+            // TODO: receptionist can only let in people in in the same gym
+            'enterance_code' => [
+                'required',
+            ]
+        ],
+    );
+    $code = $validated['enterance_code'];
+
+    return redirect()->route('let-in-2', $code);
+})->name('let-in')->middleware('auth');
+
+// List data of user page
+Route::get('/let-in/{code}', function ($code) {
+    if (!Auth::user()->is_receptionist) abort(403);
+
+    $ticket = Ticket::all()->where('code', $code)->first();
+
+    if($ticket === null) return view('receptionist.let-in'); // TODO: error message
+    if($ticket->exit !== null) return view('receptionist.let-in'); // TODO: error message
+
+    $user = $ticket->user;
+
+    $lockers = Locker::all()->where('gender', $user->gender)->where('user_id', null);
+
+    return view('receptionist.let-in-2', ['user' => $user, 'ticket' => $ticket, 'lockers' => $lockers, 'code' => $code]);
+})->name('let-in-2')->middleware('auth');
+
+// Let someone in POST
+Route::post('/let-in/{code}', function (Request $request, $code) {
+    if (!Auth::user()->is_receptionist) abort(403);
+
+    $ticket = Ticket::all()->where('code', $code)->first();
+    $user = $ticket->user;
+    $lockers = Locker::all()->where('gender', $user->gender)->where('user_id', null);
+
+    $validated = $request->validate(
+        // Validation rules
+        [
+            'locker' => [
+                'required',
+                Rule::in(Locker::all()->pluck('id'))
+            ],
+            'keyGiven' => [
+                'required',
+                'accepted'
+            ],
+        ],
+    );
+
+    $locker = Locker::all()->where('number', $validated['locker']);
+
+    $user->locker = $locker;
+
+    $enter_time = new DateTime();
+    $new_enterance = Enterance::factory()->create([
+        'gym_id' => $ticket->gym_id,
+        'user_id' => $user->id,
+        'ticket_id' => $ticket->id,
+        'enter' => $enter_time,
+        'exit' => null
+    ]);
+
+    return redirect()->route('let-in'); // TODO: success message
+})->name('let-in-2')->middleware('auth');
+
+// Let-out index page
+Route::get('/let-out', function () {
+    if (!Auth::user()->is_receptionist) abort(403);
+
+    return view('receptionist.let-in');
+})->name('let-out')->middleware('auth');
+
+// Let-out index page POST
+Route::post('/let-out', function (Request $request) {
+    if (!Auth::user()->is_receptionist) abort(403);
+
+    $validated = $request->validate(
+        [
+            // TODO: receptionist can only let in people out in the same gym
+            'enterance_code' => [
+                'required',
+            ]
+        ],
+    );
+    $code = $validated['enterance_code'];
+
+    return redirect()->route('let-in-2', $code);
+})->name('let-out')->middleware('auth');
+
+// List data of user page
+Route::get('/let-out/{code}', function ($code) {
+    if (!Auth::user()->is_receptionist) abort(403);
+
+    // TODO: write the logics here
+
+    return view('receptionist.let-out-2');
+})->name('let-out-2')->middleware('auth');
+
+// Let someone in POST
+Route::post('/let-out/{code}', function (Request $request, $code) {
+    if (!Auth::user()->is_receptionist) abort(403);
+
+    // TODO: write the logics here
+
+    return redirect()->route('let-out'); // TODO: success message
+})->name('let-out-2')->middleware('auth');
+
+/* --- Admin Routes --- */
